b17257910ec988531dcd4e9d05b596a0680cfe54
hantsy <hantsy@gmail.com>
2019-08-13 18:11:11 +0800

    more sample codes added.

--- boot/client/src/main/java/com/example/client/ClientApplication.java
+++ boot/client/src/main/java/com/example/client/ClientApplication.java
@@ -3,12 +3,16 @@ package com.example.client;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -25,22 +29,34 @@ public class ClientApplication {
 
 }
 
-@RestController
+@Slf4j
+@RestController()
 @RequiredArgsConstructor
 class GreetingController {
 
     private final RSocketRequester requester;
 
-    @GetMapping
+    @GetMapping("hello")
     Mono<Void> hello() {
         return this.requester.route("hello").data(new Greeting("Welcome to Rsocket")).send();
     }
 
+    @GetMapping("name/{name}")
+    Mono<String> greet(@PathVariable String name) {
+        return this.requester.route("greet." + name).data(new Greeting("Welcome to Rsocket")).retrieveMono(String.class);
+    }
+
+    @GetMapping(value = "stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<String> greetStream() {
+        return this.requester.route("greet-stream").data(new Greeting("Welcome to Rsocket"))
+                .retrieveFlux(String.class)
+                .doOnNext(msg->log.info("received messages::" +msg));
+    }
 }
 
 @Data
 @AllArgsConstructor
 class Greeting {
 
-    String name;
+    String message;
 }
--- boot/server/src/main/java/com/example/server/ServerApplication.java
+++ boot/server/src/main/java/com/example/server/ServerApplication.java
@@ -1,12 +1,16 @@
 package com.example.server;
 
+import java.time.Duration;
 import java.time.Instant;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -27,10 +31,24 @@ class GreetingServerController {
         log.info("received: {} at {}", p, Instant.now());
         return Mono.empty();
     }
-    
+
+    @MessageMapping("greet.{name}")
+    public Mono<String> greet(@DestinationVariable String name, @Payload Greeting p) {
+        log.info("received: {}, {} at {}", name, p, Instant.now());
+        return Mono.just("Hello " + name + ", " + p.getMessage() + " at " + Instant.now());
+    }
+
+    @MessageMapping("greet-stream")
+    public Flux<String> greetStream(@Payload Greeting p) {
+        log.info("received:  {} at {}", p, Instant.now());
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(i -> "Hello #" + i + "," + p.getMessage() + " at " + Instant.now());
+    }
+
 }
 
 @Data
 class Greeting {
-    String name;
+
+    String message;
 }
