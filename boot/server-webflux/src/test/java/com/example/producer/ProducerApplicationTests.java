149ed2197be3eb6959c967d711dd4cf786f07a56
hantsy <hantsy@gmail.com>
2019-09-05 17:45:28 +0800

    webflux sample.

---
+++ boot/server-webflux/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
