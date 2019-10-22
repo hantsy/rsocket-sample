819568a8eb7c71bd1257727a50f6d9fbae1c0a44
hantsy <hantsy@gmail.com>
2019-10-22 21:37:42 +0800

    initial server side requester

--- boot-client/src/main/java/com/example/client/ClientApplication.java
+++ boot-client/src/main/java/com/example/client/ClientApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +27,8 @@ public class ClientApplication {
 
     @Bean
     public RSocketRequester rSocketRequester(RSocketRequester.Builder b) {
-        return b.connectTcp("localhost", 7000).block();
+        return b.dataMimeType(MimeTypeUtils.APPLICATION_JSON)
+                .connectTcp("localhost", 7000).block();
     }
 
 }
---
+++ boot-server-requester/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,90 @@
+package com.example.demo;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.ConnectMapping;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public RSocketMessageHandler messageHandler() {
+        RSocketMessageHandler handler = new RSocketMessageHandler();
+        handler.setRSocketStrategies(rsocketStrategies());
+        return handler;
+    }
+
+    @Bean
+    public RSocketStrategies rsocketStrategies() {
+        return RSocketStrategies.builder()
+                .decoder(new Jackson2JsonDecoder())
+                .encoder(new Jackson2JsonEncoder())
+                .build();
+    }
+}
+
+
+@Controller
+@Slf4j
+class GreetingController {
+
+    @ConnectMapping("greet.*")
+    public void setup(RSocketRequester requester) {
+        log.info("@ConnectMapping(greet*), setup");
+    }
+
+    @MessageMapping("hello")
+    public Mono<Void> hello(Greeting p) {
+        log.info("received: {} at {}", p, Instant.now());
+        return Mono.empty();
+    }
+
+    @MessageMapping("greet.{name}")
+    public Mono<String> greet(@DestinationVariable String name, @Payload Greeting p) {
+        log.info("received: {}, {} at {}", name, p, Instant.now());
+        return Mono.just("Hello " + name + ", " + p.getMessage() + " at " + Instant.now());
+    }
+
+    @MessageMapping("greet-stream")
+    public Flux<String> greetStream(@Payload Greeting p) {
+        log.info("received:  {} at {}", p, Instant.now());
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(i -> "greet-stream#(Hello #" + i + "," + p.getMessage() + ") at " + Instant.now());
+    }
+
+    @MessageMapping("greet-channel")
+    public Flux<String> greetChannel(@Payload Flux<Greeting> p) {
+        log.info("received:  {} at {}", p, Instant.now());
+        return p.delayElements(Duration.ofSeconds(1))
+                .map(m -> "greet-channel#(" + m + ") at " + Instant.now());
+    }
+
+}
+
+@Data
+@Builder
+class Greeting {
+    String message;
+}
