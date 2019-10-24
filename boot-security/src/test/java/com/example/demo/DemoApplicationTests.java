ad968381216a4ee94465c36dfd5d86237c8be4f2
hantsy <hantsy@gmail.com>
2019-10-24 10:54:47 +0800

    clean the security test.

--- boot-security/src/test/java/com/example/demo/DemoApplicationTests.java
+++ boot-security/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.rsocket.context.RSocketServerInitializedEvent;
@@ -9,14 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.security.rsocket.metadata.BasicAuthenticationEncoder;
 import org.springframework.security.rsocket.metadata.UsernamePasswordMetadata;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.util.MimeTypeUtils;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.security.rsocket.metadata.UsernamePasswordMetadata.BASIC_AUTHENTICATION_MIME_TYPE;
 
 // see: https://github.com/rwinch/rsocket-security/blob/master/src/test/java/org/springframework/security/rsocket/itests/RSocketMessageHandlerITests.java
 // and  https://github.com/spring-projects/spring-security/blob/5.2.0.RELEASE/samples/boot/hellorsocket/src/integration-test/java/sample/HelloRSocketApplicationITests.java
@@ -28,31 +26,16 @@ class DemoApplicationTests {
     // https://github.com/spring-projects/spring-boot/pull/18287
 
     @Autowired
-    RSocketMessageHandler handler;
-
-    private RSocketRequester requester;
-
-    @BeforeEach
-    public void setup() {
-
-        this.requester = RSocketRequester.builder()
-                .dataMimeType(MimeTypeUtils.APPLICATION_JSON)
-                //				.rsocketFactory(factory -> factory.addRequesterPlugin(payloadInterceptor))
-                .rsocketStrategies(this.handler.getRSocketStrategies())
-                .connectTcp("localhost", getPort())
-                .block();
-    }
-
-    @AfterEach
-    public void dispose() {
-        this.requester.rsocket().dispose();
-    }
+    RSocketRequester.Builder requester;
 
     @Test
     public void retrieveMonoWhenSecureThenDenied() throws Exception {
+        RSocketRequester requester = this.requester
+                .connectTcp("localhost", getPort())
+                .block();
         String data = "hantsy";
         assertThatThrownBy(
-                () -> this.requester.route("greet*")
+                () -> requester.route("greet*")
                         .data(GreetingRequest.of(data))
                         .retrieveMono(GreetingResponse.class)
                         .block()
@@ -61,19 +44,17 @@ class DemoApplicationTests {
 
 
     @Test
-    public void retrieveMonoWithSetupUserSecureThenDenied() throws Exception {
+    public void retrieveMonoWithSetupUserWithoutAdminThenDenied() throws Exception {
         UsernamePasswordMetadata credentials = new UsernamePasswordMetadata("setup", "password");
 
-        this.requester = RSocketRequester.builder()
-                .dataMimeType(MimeTypeUtils.APPLICATION_JSON)
-                .setupMetadata(credentials, UsernamePasswordMetadata.BASIC_AUTHENTICATION_MIME_TYPE)
-                //				.rsocketFactory(factory -> factory.addRequesterPlugin(payloadInterceptor))
-                .rsocketStrategies(this.handler.getRSocketStrategies())
+        RSocketRequester requester = this.requester
+                .rsocketStrategies(builder -> builder.encoder(new BasicAuthenticationEncoder()))
+                .setupMetadata(credentials, BASIC_AUTHENTICATION_MIME_TYPE)
                 .connectTcp("localhost", getPort())
                 .block();
         String data = "hantsy";
         assertThatThrownBy(
-                () -> this.requester.route("greet")
+                () -> requester.route("greet")
                         .data(GreetingRequest.of(data))
                         .retrieveMono(GreetingResponse.class)
                         .block()
@@ -85,19 +66,16 @@ class DemoApplicationTests {
     public void retrieveMonoWithSetupUserAndAdminUserThenOK() throws Exception {
         UsernamePasswordMetadata credentials = new UsernamePasswordMetadata("setup", "password");
 
-        this.requester = RSocketRequester.builder()
-                .dataMimeType(MimeTypeUtils.APPLICATION_JSON)
-                .setupMetadata(credentials, UsernamePasswordMetadata.BASIC_AUTHENTICATION_MIME_TYPE)
-                //				.rsocketFactory(factory -> factory.addRequesterPlugin(payloadInterceptor))
-                .rsocketStrategies(this.handler.getRSocketStrategies())
+        RSocketRequester requester = this.requester
+                .rsocketStrategies(builder -> builder.encoder(new BasicAuthenticationEncoder()))
+                .setupMetadata(credentials, BASIC_AUTHENTICATION_MIME_TYPE)
                 .connectTcp("localhost", getPort())
                 .block();
-
         UsernamePasswordMetadata adminCredentials = new UsernamePasswordMetadata("admin", "password");
 
         String data = "hantsy";
-        this.requester.route("greet")
-                .metadata(adminCredentials, UsernamePasswordMetadata.BASIC_AUTHENTICATION_MIME_TYPE)
+        requester.route("greet")
+                .metadata(adminCredentials, BASIC_AUTHENTICATION_MIME_TYPE)
                 .data(GreetingRequest.of(data))
                 .retrieveMono(GreetingResponse.class)
                 .as(StepVerifier::create)
