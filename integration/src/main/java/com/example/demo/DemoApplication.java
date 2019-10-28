5a27a74b1e19e5f3c56ff6a476df46a0582087af
hantsy <hantsy@gmail.com>
2019-10-28 22:14:16 +0800

    updated the integration sample.

--- integration/src/main/java/com/example/demo/DemoApplication.java
+++ integration/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,18 @@
 package com.example.demo;
 
-import java.time.Duration;
-import java.time.Instant;
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.rsocket.ClientRSocketConnector;
+import org.springframework.integration.rsocket.ServerRSocketConnector;
+import org.springframework.integration.rsocket.dsl.RSockets;
+import org.springframework.integration.rsocket.outbound.RSocketOutboundGateway;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -22,40 +23,40 @@ public class DemoApplication {
 
 }
 
-@Controller
-@Slf4j
-class GreetingServerController {
+@Configuration
+class DemoIntegrationConfig {
 
-    @MessageMapping("hello")
-    public Mono<Void> hello(Greeting p) {
-        log.info("received: {} at {}", p, Instant.now());
-        return Mono.empty();
+    @Bean
+    public ServerRSocketConnector serverRSocketConnector() {
+        return new ServerRSocketConnector("localhost", 0);
     }
 
-    @MessageMapping("greet.{name}")
-    public Mono<String> greet(@DestinationVariable String name, @Payload Greeting p) {
-        log.info("received: {}, {} at {}", name, p, Instant.now());
-        return Mono.just("Hello " + name + ", " + p.getMessage() + " at " + Instant.now());
+    @Bean
+    public ClientRSocketConnector clientRSocketConnector(ServerRSocketConnector serverRSocketConnector) {
+        int port = serverRSocketConnector.getBoundPort().block();
+        ClientRSocketConnector clientRSocketConnector = new ClientRSocketConnector("localhost", port);
+        clientRSocketConnector.setAutoStartup(false);
+        return clientRSocketConnector;
     }
 
-    @MessageMapping("greet-stream")
-    public Flux<String> greetStream(@Payload Greeting p) {
-        log.info("received:  {} at {}", p, Instant.now());
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(i -> "greet-stream#(Hello #" + i + "," + p.getMessage() + ") at " + Instant.now());
+    @Bean
+    public IntegrationFlow rsocketUpperCaseRequestFlow(ClientRSocketConnector clientRSocketConnector) {
+        return IntegrationFlows
+                .from(Function.class)
+                .handle(RSockets.outboundGateway("/uppercase")
+                        .command((message) -> RSocketOutboundGateway.Command.requestStreamOrChannel)
+                        .expectedResponseType("T(java.lang.String)")
+                        .clientRSocketConnector(clientRSocketConnector))
+                .get();
     }
 
-    @MessageMapping("greet-channel")
-    public Flux<String> greetChannel(@Payload Flux<Greeting> p) {
-        log.info("received:  {} at {}", p, Instant.now());
-        return p.delayElements(Duration.ofSeconds(1))
-                .map(m -> "greet-channel#(" + m + ") at " + Instant.now());
+    @Bean
+    public IntegrationFlow rsocketUpperCaseFlow() {
+        return IntegrationFlows
+                .from(RSockets.inboundGateway("/uppercase"))
+                .<Flux<String>, Flux<String>>transform((flux) -> flux.map(String::toUpperCase))
+                .get();
     }
 
 }
 
-@Data
-class Greeting {
-
-    String message;
-}
--- integration/src/test/java/com/example/demo/DemoApplicationTests.java
+++ integration/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.function.Function;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	@Qualifier("rsocketUpperCaseRequestFlow.gateway")
+	private Function<Flux<String>, Flux<String>> rsocketUpperCaseFlowFunction;
 
+	@Test
+	void testRsocketUpperCaseFlows() {
+		Flux<String> result = this.rsocketUpperCaseFlowFunction.apply(Flux.just("a\n", "b\n", "c\n"));
+
+		StepVerifier.create(result)
+				.expectNext("A", "B", "C")
+				.verifyComplete();
+	}
 }
