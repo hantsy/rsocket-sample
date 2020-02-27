7868883e2ef88529a5704417f5af7a0a79d93dcf
hantsy <hantsy@gmail.com>
2020-02-27 18:06:38 +0800

    update the integration sample

--- integration/src/main/java/com/example/demo/DemoApplication.java
+++ integration/src/main/java/com/example/demo/DemoApplication.java
@@ -1,30 +1,36 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.MediaType;
+import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.rsocket.ClientRSocketConnector;
+import org.springframework.integration.rsocket.RSocketInteractionModel;
 import org.springframework.integration.rsocket.ServerRSocketConnector;
 import org.springframework.integration.rsocket.dsl.RSockets;
-import org.springframework.integration.rsocket.outbound.RSocketOutboundGateway;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MimeType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.net.URI;
 import java.util.function.Function;
 
 @SpringBootApplication
+@EnableIntegration
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-}
-
-@Configuration
-class DemoIntegrationConfig {
 
     @Bean
     public ServerRSocketConnector serverRSocketConnector() {
@@ -44,7 +50,7 @@ class DemoIntegrationConfig {
         return IntegrationFlows
                 .from(Function.class)
                 .handle(RSockets.outboundGateway("/uppercase")
-                        .command((message) -> RSocketOutboundGateway.Command.requestStreamOrChannel)
+                        .interactionModel((message) -> RSocketInteractionModel.requestChannel)
                         .expectedResponseType("T(java.lang.String)")
                         .clientRSocketConnector(clientRSocketConnector))
                 .get();
@@ -53,10 +59,24 @@ class DemoIntegrationConfig {
     @Bean
     public IntegrationFlow rsocketUpperCaseFlow() {
         return IntegrationFlows
-                .from(RSockets.inboundGateway("/uppercase"))
+                .from(RSockets.inboundGateway("/uppercase")
+                        .interactionModels(RSocketInteractionModel.requestChannel))
                 .<Flux<String>, Flux<String>>transform((flux) -> flux.map(String::toUpperCase))
                 .get();
     }
 
 }
 
+@RestController
+class HelloController {
+
+    @Autowired()
+    @Lazy
+    @Qualifier("rsocketUpperCaseRequestFlow.gateway")
+    private Function<Flux<String>, Flux<String>> rsocketUpperCaseFlowFunction;
+
+    @GetMapping(value = "hello", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> uppercase() {
+        return rsocketUpperCaseFlowFunction.apply(Flux.just("a", "b", "c", "d"));
+    }
+}
--- integration/src/test/java/com/example/demo/DemoApplicationTests.java
+++ integration/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,24 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.util.function.Function;
 
-@SpringBootTest
+@ContextConfiguration(classes = DemoApplication.class)
 class DemoApplicationTests {
 
-	@Autowired
-	@Qualifier("rsocketUpperCaseRequestFlow.gateway")
-	private Function<Flux<String>, Flux<String>> rsocketUpperCaseFlowFunction;
+    @Autowired
+    @Qualifier("rsocketUpperCaseRequestFlow.gateway")
+    private Function<Flux<String>, Flux<String>> rsocketUpperCaseFlowFunction;
 
-	@Test
-	void testRsocketUpperCaseFlows() {
-		Flux<String> result = this.rsocketUpperCaseFlowFunction.apply(Flux.just("a\n", "b\n", "c\n"));
+    @Test
+    void testRsocketUpperCaseFlows() {
+        Flux<String> result = this.rsocketUpperCaseFlowFunction.apply(Flux.just("a\n", "b\n", "c\n"));
 
-		StepVerifier.create(result)
-				.expectNext("A", "B", "C")
-				.verifyComplete();
-	}
+        StepVerifier.create(result)
+                .expectNext("A", "B", "C")
+                .verifyComplete();
+    }
 }
